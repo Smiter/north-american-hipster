f35471e67dd86ae4aa8d1d8449eae8c62f1996af
Smiter <smiter4@gmail.com>
2013-11-10 22:28:39 -0500

    added dushboard page

--- urls.py
+++ urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
+    url(r'^dashboard/', TemplateView.as_view(template_name='dashboard.html'), name="dashboard"),
     url(r'^admin/', include(admin.site.urls)),
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.STATIC_ROOT}),
