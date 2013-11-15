965cc2fc24362f3cd0813926000bfcd0f48eb60d
Smiter <smiter4@gmail.com>
2013-11-14 22:22:23 -0500

    added simple test output of hubs, added requeirments

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from pymongo import MongoClient
 
-# Create your views here.
+def dashboard(request):
+    result = [{'hubname': 'atlanta'}, {'hubname': 'mexico'}]
+    return render_to_response('dashboard.html', {"result": result})
+
--- urls.py
+++ urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
 from django.conf import settings
+from mainsite import views as mainsite_views
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
-    url(r'^dashboard/', TemplateView.as_view(template_name='dashboard.html'), name="dashboard"),
+    url(r'^dashboard/', mainsite_views.dashboard),
     url(r'^admin/', include(admin.site.urls)),
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.STATIC_ROOT}),
