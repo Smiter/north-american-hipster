348916879d78edefa183e003fe6e1e3f2de00f91
Smiter <smiter4@gmail.com>
2013-11-17 21:37:41 -0500

    added view of current social media for choosed hub

--- mainsite/views.py
+++ mainsite/views.py
@@ -6,6 +6,8 @@ client = MongoClient()
 db = client.socialdb
 hubs = db.hubs
 
+SUPPORTED_SOCIAL = ["twitter", "facebook", "instagram"]
+
 def dashboard(request):
     return render_to_response('dashboard.html', {"result": hubs.find()})
 
@@ -13,4 +15,9 @@ def view_hub(request, hubname):
     return render_to_response('view_hub.html', {"hubname": hubname}) 
 
 def edit_hub(request, hubname):
-    return render_to_response('edit_hub.html', {"hubname": hubname})      
+    hub = hubs.find_one({"hubname": hubname})
+    social_accounts = []
+    for i in hub.items():
+        if i[0] in SUPPORTED_SOCIAL:
+            social_accounts.append(i)
+    return render_to_response('edit_hub.html', {"hubname": hubname, "social_accounts": social_accounts})      
